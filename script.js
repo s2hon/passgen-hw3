bed4e4d8a5d88ddd1753c94101bb575c0314f9c5
Staci Shon <staci.shon@gmail.com>
2020-09-23 20:20:50 -0500

    reorganized the flow of JS 1/3 done

--- script.js
+++ script.js
@@ -1,47 +1,103 @@
+//components//
+// random # // console.log (Math.floor(Math.random()));
+  //alphabet.length 26
+  var alphabet = ["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  //numeric.length 10
+  var numeric = [0,1,2,3,4,5,6,7,8,9];
+  //specialChar.length 33 
+  var specialChar=["\ ","\!","\"","\#","\$","\%","\&","\'","\(","\)","\*","\+","\,","\-","\.","\/","\:","\;","\<","\=","\>","\?","\@","\[","\\","\]","\^","\_","\`","\{","\|","\}","\~"];
+  
+
+//8 characters and no more than 128 characters
+  var userLength = parseInt (prompt ("How long do you want your password? Please type a # between 8 characters and 128"));
+//needs to enter right value to proceed
+  if (userLength<8 || 128<userLength) {
+    alert ("please enter a valid number.");
+    return
+  }
+
+  else if (isNaN(userLength) === true) {
+    alert ("please enter a valid number.");
+    return
+  }
+  else {
+    alert ("please choose at least ONE from the following options. Hit ok to countinue");
+     // lowercase:
+  var userLC = confirm ("Do you want lowercase?");
+    
+  // uppercase:
+  var userUC = confirm ("Do you want uppercase?");
+  
+  // numeric:
+  var userNumeric = confirm ("Do you want numeric?");
+  
+  // specialChar:
+  var userSpecChar = confirm ("Do you want special character?");
+  };
+
+  //to prevent user error
+  if (userLC === false && userUC === false && userNumeric === false && userSpecChar === false) {
+    alert ("you must choose at least ONE option.");
+  }
+
+  else {
+
+  }
+
 // Assignment Code 
 var generateBtn = document.querySelector("#generate");
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
+  //end result
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-//click the button
+//where I start//
 
 
 
+  var genRandom = {function randomUpper () {
 
+    return console.log (alphabet[Math.floor(Math.random()*26)]);
+   
+   }
+   
+   function randomLower () {
+   
+     return console.log ((alphabet[Math.floor(Math.random()*26)]).toLowerCase());
+   
+   }
+   
+   function randomNumeric () {
+     return console.log (numeric[Math.floor(Math.random()*10)]);
+   }
+   
+   function randomSpChar () {
+     return console.log (specialChar[Math.floor(Math.random()*33)]);
+   }
 
-
-//components//
-
-var requirement = {
-
-  //8 characters and no more than 128 characters
-  passSize: function () {
-    var userInput = prompt ("How long do you want your password? Please type a # between 8 characters and 128");
-    this.passSize = parseInt(userInput)
   }
 
-  // lowercase:
-  confirm ("Do you want lowercase?")
-    
-  // uppercase:
-  confirm ("Do you want uppercase?")
 
-  // numeric:
-  confirm ("Do you want numeric?")
 
-  // specialChar:
-  confirm ("Do you want special character?")
+// Generator Functions
+
+function generatePassword() {
+
+
 }
+
+
 while (passSize >=8 && passSize <=128){
 
   
@@ -67,41 +123,8 @@ function reWriteStats() {
    console.log ("-----------------")
  }
 
-// Generator Functions
 
-// random # // console.log (Math.floor(Math.random()));
-  //alphabet.length 26
-  //numeric.length 10
-  //specialChar.length 32
 
-function randomLower () {
 
-  console.log (Math.floor(Math.random()*26))
-
-}
-
-function randomUpper () {
-
-  console.log (Math.floor(Math.random()*26))
-
-}
-
-function randomNumeric () {
-  console.log (Math.floor(Math.random()*10))
-}
-
-function randomChar () {
-  console.log (Math.floor(Math.random()*32))
-}
-
-// var alphabet = ["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-// var numeric = [0,1,2,3,4,5,6,7,8,9];
-// var specialChar=["~`!#$%^&*+=-[]\';,/{}|\":<>?";]
-// var lowercase [.]
-// var uppercase []
-
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
 //WHEN the password is generated
 //THEN the password is either displayed in an alert or written to the page//
